12b250d6e19789f16dc90f6574f9fb1c27751625
Ricardo Rubio <rickarubio@gmail.com>
2014-02-08 17:54:07 -0800

    Adds ability to create Todos via the input text field.

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -4,6 +4,6 @@ get '/' do
 end
 
 post '/add_todo' do
-  p "Inside /add_todo route!"
+  Todo.create(params)
 end
 
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -4,10 +4,26 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+    $('.add').on('click', function(e) {
+      e.preventDefault();
+      addTodo();
+    });
   }
 
   //Create functions to add, remove and complete todos
-
+  function addTodo() {
+    $.ajax({
+      type: "POST",
+      url: '/add_todo',
+      data: {'todo_content': $('input.todo').val()}
+    })
+    .done(function() {
+      $('input.todo').val('');
+    })
+    .fail(function() {
+      console.log('ajax post to /add_todo failed');
+    });
+  };
 
 
   function buildTodo(todoName) {

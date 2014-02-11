5f19ba3b6e9da073e9c7f607fc54cfc401d54ad4
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-11 01:00:17 -0800

    Implements jQuery UI to make items draggable

--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -19,50 +19,20 @@ function bindEvents() {
     event.preventDefault;
     completeTodo($(this));
   })
-  // binds dragstart
-  $('.todo_list').on('dragstart', 'li', function(event) {
-    event.preventDefault;
-    handleDragStart(event);
-  })
-  // binds drag
-  $('.todo_list').on('drag', 'li', function(event) {
-    event.preventDefault;
-  })
-  // binds dragenter
-  $('.todo_list').on('dragenter', 'li', function(event) {
-    event.preventDefault;
-  })
-  // binds dragleave
-  $('.todo_list').on('dragleave', 'li', function(event) {
-    event.preventDefault;
-  })
-  // binds dragover
-  $('.todo_list').on('dragover', 'li', function(event) {
-    event.preventDefault;
-  })
-  // binds drop
-  // $('.todo_list').on('drop', 'li', function(event) {
-  //   event.preventDefault;
-  //   console.log("drop!!!");
-  //   handleDrop(event);
-  // })
-  // binds dragend
-  $('.todo_list').on('dragend', 'li', function(event) {
-    event.preventDefault;
-  })
 }
 
 function addTodo(todo) {
   var formData = todo.serialize();
   $.post('/add_todo', formData, function(returnData) {
     displayNewTodo(returnData);
+    $('.todo_list li').draggable();
   });
 }
 
 function displayNewTodo(dataFromServer) {
-  $('.todo_list').append('<li draggable=true><h2>' + dataFromServer.todo.todo_content + '</h2> <a class="delete" data-num="' 
+  $('.todo_list').append('<div class="draggable"><li><h2>' + dataFromServer.todo.todo_content + '</h2> <a class="delete" data-num="' 
       + dataFromServer.todo.id + '"href="#">Delete</a> <a class="complete" data-num="' 
-      + dataFromServer.todo.id + '"href="#">Complete</a> </li>');
+      + dataFromServer.todo.id + '"href="#">Complete</a> </li></div>');
 }
 
 function deleteTodo(todo) {
@@ -95,21 +65,3 @@ function completeTodo(todo) {
 function modifyTodo(dataFromServer) {
   $('a[data-num=' + dataFromServer.todo.id + ']').parent().addClass('completed');
 }
-
-function handleDragStart(e) {
-  dragSrcEl = this;
-  e.originalEvent.dataTransfer.effectAllowed = 'move';
-  e.originalEvent.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-// function handleDrop(e) {
-//   if (e.stopPropagation){
-//     e.stopPropagation();
-//   }
-
-//   if (dragSourceElement != this){
-//     dragSourceElement.innerHTML = this.innerHTML;
-//     this.innerHTML = e.originalEvent.dataTransfer.getData('text/html');
-//   }
-//   return false;
-// }

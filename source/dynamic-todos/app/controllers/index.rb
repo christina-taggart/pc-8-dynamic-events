4135351563b7ab986dc15fd2030767d023d00aa8
Alexander Leishman <null>
2014-02-10 23:57:26 -0800

    included more files

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,9 +1,22 @@
 get '/' do
   # Look in app/views/index.erb
+  @todos = Todo.all
   erb :index
 end
 
 post '/add_todo' do
-  p "Inside /add_todo route!"
+  p params
+  @todo = Todo.create(params)
+  erb :list_element, layout: false
+end
+
+put '/todos/:id' do
+  todo = Todo.find(params[:id])
+  todo.update_attributes(completed: true)
+end
+
+delete '/todos/:id' do
+  t = Todo.find(params[:id])
+  t.destroy
 end
 
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -1,24 +1,107 @@
 $(document).ready(function() {
+
   var todoTemplate = $.trim($('#todo_template').html());
 
   function bindEvents() {
-    // Bind functions which add, remove, and complete todos to the appropriate
-    // elements
+
+    $('form').submit(function(e){
+      e.preventDefault();
+      $.post('/add_todo', $(this).serialize()).done(function(response){
+        $(".todo_list").append(response);
+      });
+    })
+
+    $(".todo_list").on('click', '.delete', function(e){
+        var $this = $(this)
+        var todo_id = $this.data('id');
+        $.ajax({
+          url: 'todos/' + todo_id,
+          type: 'DELETE',
+        }).done(function(response) {
+          $this.closest('div').fadeOut();
+          $this.remove();
+        })
+    })
+
+    $(".todo_list").on('click', '.complete', function(e){
+        var $this = $(this)
+        var todo_id = $this.data('id');
+        $.ajax({
+          url: 'todos/' + todo_id,
+          type: 'put',
+        }).done(function(response) {
+          $this.closest('div').addClass('complete')
+        })
+    })
   }
 
-  //Create functions to add, remove and complete todos
+  function handleDrags(){
+
+    var dragSrcEl = null;
+
+    function handleDragStart(e) {
+      this.style.opacity = '0.5';
+
+      dragSrcEl = this;
+
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.innerHTML);
+    }
 
 
+    function handleDragOver(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      e.dataTransfer.dropEffect = 'move';
+      return false;
+    }
 
-  function buildTodo(todoName) {
-    // Creates an jQueryDOMElement from the todoTemplate.
-    var $todo = $(todoTemplate);
-    // Modifies it's text to use the passed in todoName.
-    $todo.find('h2').text(todoName);
-    // Returns the jQueryDOMElement to be used elsewhere.
-    return $todo;
+    function handleDragEnter(e) {
+      if(this.classList[0] == "todo"){
+        this.classList.add('over');
+      }
+    }
+
+    function handleDragLeave(e) {
+      this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+
+      if (e.stopPropagation) {
+        e.stopPropagation();
+      }
+
+      if (dragSrcEl != this){
+
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+        this.style.opacity = '1';
+      }
+
+      return false;
+    }
+
+    function handleDragEnd(e) {
+      [].forEach.call(todos, function(todo){
+        todo.classList.remove('over');
+      })
+    }
+
+    var todos = document.querySelectorAll('.todo_list .todo'); // necessary?
+
+    [].forEach.call(todos, function(todo) {
+      todo.addEventListener('dragstart', handleDragStart, false);
+      todo.addEventListener('dragenter', handleDragEnter, false);
+      todo.addEventListener('dragover', handleDragOver, false);
+      todo.addEventListener('dragleave', handleDragLeave, false);
+      todo.addEventListener('drop', handleDrop, false);
+      todo.addEventListener('dragend', handleDragEnd, false);
+    });
   }
 
 
   bindEvents();
+  handleDrags();
 });

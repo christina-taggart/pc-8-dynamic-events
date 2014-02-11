b3e0ce689cbab49ab3d0e86b2043bffd767adaf6
K Y <kevin@kevinyeh.com>
2014-02-11 01:48:02 -0800

    Add routes for add update and delete todo

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -3,7 +3,25 @@ get '/' do
   erb :index
 end
 
-post '/add_todo' do
+post '/todos' do
+	content_type :json
   p "Inside /add_todo route!"
+  new_todo = params[:new_todo]
+  @todo = Todo.create(new_todo)
+  @todo.to_json
 end
 
+put '/todos/:id' do
+	content_type :json
+  p "Inside /add_todo route!"
+  @new_todo = Todo.find(params[:id])
+  new_todo = params[:new_todo]
+  @todo = Todo.create(new_todo)
+  @todo.to_json
+end
+
+delete '/todos/:id' do
+	@todo = Todo.find(params[:id])
+	@todo.destroy
+	params[:id]
+end

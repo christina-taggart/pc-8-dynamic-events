2655f79d2d601b48c3aa89a23ce2e616dafef81a
Apprentice <apprentice@dbc15.local>
2014-02-11 20:58:31 -0800

    User can add, complete, and delete todos

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -4,6 +4,23 @@ get '/' do
 end
 
 post '/add_todo' do
-  p "Inside /add_todo route!"
+  content_type :json
+  p params
+  Todo.create(params)
+  @todo = Todo.last
+  @todo.to_json
 end
 
+delete '/todo/:id' do
+  content_type :json
+  @todo = Todo.find(params[:id])
+  Todo.destroy(params[:id])
+  @todo.to_json
+end
+
+put '/todo/:id' do
+  content_type :json
+  @todo = Todo.find(params[:id])
+  Todo.update(params[:id], :completed => true)
+  @todo.to_json
+end
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -4,11 +4,51 @@ $(document).ready(function() {
   function bindEvents() {
     // Bind functions which add, remove, and complete todos to the appropriate
     // elements
+    $('#create_todo').on('submit', addTodo)
+    $('.todo_list').on('click', '.delete', deleteTodo)
+    $('.todo_list').on('click', '.complete', completeTodo)
+    $('.completed_list').on('click', '.delete', deleteTodo)
+
+}
+  function addTodo(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/add_todo",
+      data: $(this).serialize(),
+    }).done(appendTodo);
   }
 
-  //Create functions to add, remove and complete todos
+  function appendTodo(erb) {
+    $(".todo_list").append("<div class='list_item item_" + erb.todo.id + "'><li>" + erb.todo.todo_content + "</li><a href='/todo/" + erb.todo.id + " ' class='delete'>Delete</a> <a href='/todo/" + erb.todo.id + " ' class='complete'>Complete</a></div>")
+  }
 
+  function deleteTodo(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "DELETE",
+      url: this.href
+    }).done(removeTodo);
+  }
 
+  function removeTodo(erb) {
+    var $element = $(".item_" + erb.todo.id);
+    $element.remove();
+  }
+
+  function completeTodo(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "PUT",
+      url: this.href
+    }).done(finishedTodo);
+  }
+
+  function finishedTodo(erb) {
+    var $element = $(".item_" + erb.todo.id)
+    $element.find('.complete').remove()
+    $element.appendTo(".completed_list")
+  }
 
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.

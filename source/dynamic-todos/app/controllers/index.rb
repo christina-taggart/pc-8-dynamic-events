cd5da74e96b3dee9032d510bd99e1c585aac4519
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-02-11 22:41:59 -0800

    Release 0: Add todo to the page

--- source/dynamic-todos/app/controllers/index.rb
+++ source/dynamic-todos/app/controllers/index.rb
@@ -1,9 +1,20 @@
 get '/' do
   # Look in app/views/index.erb
+  @todos = Todo.all
   erb :index
 end
 
-post '/add_todo' do
-  p "Inside /add_todo route!"
+post '/todo_list' do
+  content_type :json
+  @new_todo = Todo.create(params)
+  {todo_content: @new_todo.todo_content, id: @new_todo.id}.to_json
 end
+
+delete '/todo_list/:id' do
+  content_type :json
+  @delete_todo = Todo.find(params[:id])
+  @delete_todo.destroy
+  {id: @delete_todo.id}.to_json
+end
+
 
--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -7,18 +7,49 @@ $(document).ready(function() {
   }
 
   //Create functions to add, remove and complete todos
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      method: 'post',
+      url: '/todo_list',
+      data: $(this).serialize()
+    }).done( function(response){
+      // $('.todo_list').append(response.todo_content);
+      var $todo = buildTodo(response);
+      $('.todo_list').append($todo);
+    })
+  })
+
+  $('.delete').on('click', function(event){
+    debugger;
+    event.preventDefault();
+    console.log(this);
+    console.log("delete!!!");
+    $.ajax({
+      method: 'delete',
+      url: '/todo_list/' + $('a.delete').data('id')
+    }).done( function(response){
+    debugger;
+      deleteTodo(response.id);
+    })
+  })
 
 
-
-  function buildTodo(todoName) {
+  function buildTodo(todo) {
     // Creates an jQueryDOMElement from the todoTemplate.
     var $todo = $(todoTemplate);
     // Modifies it's text to use the passed in todoName.
-    $todo.find('h2').text(todoName);
+    $todo.find('#content').text(todo.todo_content);
     // Returns the jQueryDOMElement to be used elsewhere.
+    $todo.find('a.delete').data('id', todo.id);
     return $todo;
   }
 
+  function deleteTodo(todo_id) {
+    var $todo = $(todoTemplate);
+    $todo.find(todo_id).remove();
+    return $todo;
+  }
 
   bindEvents();
 });
